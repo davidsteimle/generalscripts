349c33a7351774252a1c794d7f051b0cbae464f2
David Steimle <21266257+davidsteimle@users.noreply.github.com>
2019-10-04 08:32:13 -0400

    Create baseinstall.sh

---
+++ baseinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+# install lynx text browsers
+sudo apt-get install lynx -y
+# install Apache, mySQL, PHP as lamp-server
+sudo apt-get install lamp-server^
