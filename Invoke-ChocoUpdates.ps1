539fdd8b2cb26ed93de01e78ac41ec1b836734bd
David Steimle <davidsteimle@gmail.com>
2020-08-09 13:43:08 -0400

    Update Invoke-ChocoUpdates.ps1

--- Invoke-ChocoUpdates.ps1
+++ Invoke-ChocoUpdates.ps1
@@ -15,22 +15,7 @@ function Invoke-ChocoUpdates{
     [CmdletBinding()]
     Param()
 
-    $TestChocoOutdated = [array]$(choco outdated)
+    $TestChocoOutdated = Get-ChocoOutdated
 
-    $OutdatedList = New-Object "System.Collections.Generic.List[PSObject]"
-
-    $TestChocoOutdated.ForEach({
-        if($PSItem -match "[\|]" -and $PSItem -notmatch "[\?]"){
-            $ResultArray = $PSItem.Split('|')
-            $ItemHash = [ordered]@{
-                SoftwareName = $ResultArray[0]
-                CurrentVersion = $ResultArray[1]
-                AvailableVersion = $ResultArray[2]
-                Pinned = $(if($ResultArray[3] -match 'true'){$true} else {$false})
-            }
-            $OutdatedList.Add($(New-Object psobject -Property $ItemHash))
-        }
-    })
-
-    $OutdatedList
+    
 }
